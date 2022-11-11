13873addb16964ad12a0f23f77fa2a4e9e82db98
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-11-11 00:16:55 +0000

    add cassandra docker container

--- run.sh
+++ run.sh
@@ -2,3 +2,8 @@
 docker build -t frontend-server ./frontend-server
 # run frontend server container
 docker run -d -p 5000:5000 --name heterogeneous-faas-frontend-server frontend-server:latest
+
+# run resource-manager container
+
+# run cassandra DB container
+docker run --name cass_cluster cassandra:latest
