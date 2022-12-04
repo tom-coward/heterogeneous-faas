13ad6854b9f1879a4cef4cb67726c29efab8b0ff
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2022-12-04 20:42:37 +0000

    Update Kubernetes.java

--- resource-manager/app/src/main/java/com/tomcoward/heterogeneousfaas/resourcemanager/integrations/Kubernetes.java
+++ resource-manager/app/src/main/java/com/tomcoward/heterogeneousfaas/resourcemanager/integrations/Kubernetes.java
@@ -5,17 +5,13 @@ import com.tomcoward.heterogeneousfaas.resourcemanager.exceptions.IntegrationExc
 import com.tomcoward.heterogeneousfaas.resourcemanager.models.Function;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.ApiResponse;
 import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.BatchV1Api;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1Job;
 import io.kubernetes.client.openapi.models.V1Namespace;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.Watch;
 import javax.json.JsonObject;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
