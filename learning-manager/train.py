527c118c66735f98b94a8e82428e83586f257834
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2023-02-19 17:00:25 +0000

    add training of linear regression model

--- learning-manager/train.py
+++ learning-manager/train.py
@@ -1,14 +1,35 @@
 from cassandra.cluster import Cluster
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 cassandraCluster = Cluster(['localhost'])
 cassandraSession = cassandraCluster.connect()
 
 def getFunctionExecutions(function_name: str):
-    functionExecutions = cassandraSession.execute(f"SELECT input_size, duration FROM heterogeneous_faas.function_execution WHERE function_name='{function_name}'")
+    rows = cassandraSession.execute(f"SELECT input_size, duration FROM heterogeneous_faas.function_execution WHERE function_name='{function_name}'")
 
-    return functionExecutions
+    inputSizes = []
+    durations = []
+
+    for row in rows:
+        inputSizes.append(row.column1)
+        durations.append(row.column2)
+
+    return inputSizes, durations
 
 def train(function_name: str):
-    functionExecutions = getFunctionExecutions(function_name)
+    inputSizes, durations = getFunctionExecutions(function_name)
 
-    print(functionExecutions)
+    inputSizes = np.array(inputSizes)
+    durations = np.array(durations)
+
+    X = inputSizes.reshape(-1, 1) 
+    y = durations[:, 1]
+
+    model = LinearRegression().fit(X, y)
+
+    print("Coefficients: ", model.coef_)
+    print("Intercept: ", model.intercept_)
+
+cassandraSession.shutdown()
+cassandraCluster.shutdown()
