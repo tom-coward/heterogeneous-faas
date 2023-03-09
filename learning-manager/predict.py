b35b1961859d76213446f9241607b969ffeed3cc
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2023-03-09 21:31:04 +0000

    add ML predictions

--- learning-manager/predict.py
+++ learning-manager/predict.py
@@ -1,4 +1,29 @@
-def getPredictions(functionName: str):
+from cassandra.cluster import Cluster
+import pickle
+
+cassandraCluster = Cluster(['localhost'])
+cassandraSession = cassandraCluster.connect()
+
+def getMLModels(functionName: str):
+    rows = cassandraSession.execute(f"SELECT worker_id, model FROM heterogeneous_faas.ml_model WHERE function_name='{functionName}'")
+
+    models = []
+
+    for row in rows:
+        models.append([row.worker_id, row.model])
+
+    return models
+
+def getPredictions(functionName: str, inputSize: int):
     predictions = []
+    
+    models = getMLModels(functionName)
+
+    for m in models:
+        model = pickle.load(m[1])
+
+        prediction = model.predict([[inputSize]])
+
+        predictions.append([m[0], prediction])
 
     return predictions
