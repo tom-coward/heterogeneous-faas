fedcf62ce7526f48ab710209a1d825af6f936b66
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2023-03-13 23:23:15 +0000

    cleanup of project-codenet/main.py

--- project-codenet/main.py
+++ project-codenet/main.py
@@ -2,7 +2,6 @@ import compiler
 import uploader
 import inputgenerator
 import argparse
-import subprocess
 
 
 # select random solutions to selected problems (as defined by their ID below) in the chosen languages
