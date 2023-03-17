fd4a62b62d3fb50e342dbde31e4ce77d94aeeadd
Tom Coward <34864926+tom-coward@users.noreply.github.com>
2023-03-17 00:36:53 +0000

    switch to SGDRegressor

--- learning-manager/train.py
+++ learning-manager/train.py
@@ -26,13 +26,17 @@ def getFunctionExecutions(functionName: str, worker: str):
 
 def getMlModel(functionName: str, worker: str):
     # TODO: remove ALLOW FILTERING if possible
-    result = cassandraSession.execute(f"SELECT model FROM heterogeneous_faas.ml_model WHERE function_name='{functionName}' AND worker = '{worker}' ALLOW FILTERING")
+    result = cassandraSession.execute(f"SELECT id, model FROM heterogeneous_faas.ml_model WHERE function_name='{functionName}' AND worker = '{worker}' ALLOW FILTERING")
 
-    return result.one().model
+    return result.one()
 
 def saveModel(functionName: str, worker: str, modelBytes: bytes):
     saveModelStatement = cassandraSession.prepare(f"INSERT INTO heterogeneous_faas.ml_model (id, function_name, worker, model) VALUES (?, ?, ?, ?)")
     cassandraSession.execute(saveModelStatement, (uuid.uuid4(), functionName, worker, modelBytes))
+
+def updateModel(id, functionName: str, worker: str, modelBytes: bytes):
+    saveModelStatement = cassandraSession.prepare(f"INSERT INTO heterogeneous_faas.ml_model (id, function_name, worker, model) VALUES (?, ?, ?, ?)")
+    cassandraSession.execute(saveModelStatement, (id, functionName, worker, modelBytes))
 
 def train(functionName: str):
     for worker in workers:    
@@ -60,7 +64,7 @@ def train(functionName: str):
         # create linear regression model
         regressor = make_pipeline(
             StandardScaler(),
-            SGDRegressor(eta0=0.01, learning_rate="constant", random_state=42)
+            SGDRegressor(loss="squared_error", alpha=0.0001)
         )
 
         x = numpy.array(data[:, 0]).reshape(-1, 1)
@@ -68,13 +72,14 @@ def train(functionName: str):
 
         regressor.fit(x, y)
 
-        # plot original model as graph
-        plt.scatter(x, y)
-        plt.plot(x, regressor.predict(x), color='red')
-        plt.title(f'{worker} Model')
-        plt.xlabel('Input size')
-        plt.ylabel('Duration')
-        plt.show()
+        # plot original model as graph (if not running through Flask due to thread issues)
+        if __name__ == "__main__":
+            plt.scatter(x, y)
+            plt.plot(x, regressor.predict(x), color='red')
+            plt.title(f'{worker} Model')
+            plt.xlabel('Input size')
+            plt.ylabel('Duration')
+            plt.show()
 
         # save model in Cassandra database (as a string representation of model object)
         modelBytes = pickle.dumps(regressor)
@@ -83,12 +88,31 @@ def train(functionName: str):
 def incrementalTrain(functionName: str, worker: str, inputSize: int, duration: float):
     # get existing model
     mlModel = getMlModel(functionName, worker)
-    regressor = pickle.loads(mlModel)
+    regressor = pickle.loads(mlModel.model)
+
+    newX = numpy.array([inputSize]).reshape(-1, 1)
+    newY = numpy.array([duration])
     
     # fit new data (input size & duration) to model
-    regressor.named_steps['sgdregressor'].partial_fit(inputSize, duration)
+    regressor.named_steps['sgdregressor'].partial_fit(newX, newY)
+
+    # save model in Cassandra database (as a string representation of model object)
+    modelBytes = pickle.dumps(regressor)
+    updateModel(mlModel.id, functionName, worker, modelBytes)
+
 
 
 if __name__ == '__main__':
+    mode = input("Enter mode (T = train or I = incremental train): ")
+
     functionName = input("Enter name of function to train: ")
-    train(functionName)
+
+    if mode == "T":
+        train(functionName)
+
+    if mode == "I":
+        worker = input("Enter worker: ")
+        inputSize = int(input("Enter input size: "))
+        duration = float(input("Enter duration: "))
+
+        incrementalTrain(functionName, worker, inputSize, duration)
